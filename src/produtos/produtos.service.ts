1e11df59111ff43feff496497f852bb5033cc540
gabrielBartolace <gabrielll.freitas@hotmail.com>
2024-07-04 17:54:11 -0400

    repository produto

--- src/produtos/produtos.service.ts
+++ src/produtos/produtos.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProdutoDto } from './dto/create-produto.dto';
 import { UpdateProdutoDto } from './dto/update-produto.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ProdutosService {
+
+  constructor(private prismaService: PrismaService){}
+
   create(createProdutoDto: CreateProdutoDto) {
+    // this.prismaService.produto.
     return 'This action adds a new produto';
   }
 
   findAll() {
-    return `This action returns all produtos`;
+    
+    return this.prismaService.produto.findMany();
   }
 
   findOne(id: number) {
