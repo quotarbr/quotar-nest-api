ba40fdcd62179ebef8b2d45a6f8be40413bb35f5
Gabriel Bartolace <gabrielll.freitas@hotmail.com>
2024-07-26 07:33:20 -0400

    update lojista crud

---
+++ src/lojistas/dto/lojista.dto.ts
@@ -0,0 +1,17 @@
+import { LOJST_STATUS } from "../enums/lojst-status.enum";
+
+export class CreateLojistaDto {
+    lojst_nome:                 string;
+    lojst_cpf:                  string;
+    lojst_img_perfil?:          string;
+    lojst_telefone:             string;
+    lojst_email:                string;
+    lojst_data_cadastro:        Date;
+    lojst_cep?:                 string;
+    lojst_endereco?:            string;
+    lojst_status:               LOJST_STATUS;
+    cid_id:                     number;
+    bai_id:                     number;
+    est_id:                     number;
+    lojst_loja_parceira?:       string;
+}
--- src/lojistas/lojistas.service.ts
+++ src/lojistas/lojistas.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateLojistaDto } from './dto/create-lojista.dto';
+
 import { UpdateLojistaDto } from './dto/update-lojista.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { Prisma } from '@prisma/client';
@@ -9,26 +10,44 @@ export class LojistasService {
   constructor(
     private prismaService: PrismaService
   ){}
-  create(createLojistaDto: CreateLojistaDto) {
-    const data: Prisma.LojistaUncheckedCreateInput = {
-      lojst_nome:               createLojistaDto.lojst_nome,
-      lojst_cpf:                createLojistaDto.lojst_cpf,
-      lojst_img_perfil:         createLojistaDto.lojst_img_perfil,
-      lojst_telefone:           createLojistaDto.lojst_telefone,
-      lojst_email:              createLojistaDto.lojst_email,
-      lojst_cep:                createLojistaDto.lojst_cep,
-      lojst_endereco:           createLojistaDto.lojst_endereco,
-      lojst_status:             createLojistaDto.lojst_status,
-      lojst_loguin:             createLojistaDto.lojst_loguin,
-      lojst_senha_hash:         createLojistaDto.lojst_senha_hash,
-      lojst_token_inspiracao:   createLojistaDto.lojst_token_inspiracao,
-      lojst_token_recuperacao:  createLojistaDto.lojst_token_recuperacao,
-      cid_id:                   createLojistaDto.cid_id,
-      bai_id:                   createLojistaDto.bai_id,
-      est_id:                   createLojistaDto.est_id,
-      lojst_loja_parceira:      createLojistaDto.lojst_loja_parceira
+  async create(createLojistaDto: CreateLojistaDto) {
+    const hasUser = await this.prismaService.lojista.findFirst({
+      where: {
+        lojst_email: createLojistaDto.lojst_email,
+        lojst_telefone: createLojistaDto.lojst_telefone
+      }
+    })
+
+    if(hasUser) {
+      throw new BadRequestException("Usuário já cadastrado");
+    }
+
+    const data = {
+      // lojst_nome:               createLojistaDto.lojst_nome,
+      // lojst_cpf:                createLojistaDto.lojst_cpf,
+      // lojst_img_perfil:         createLojistaDto.lojst_img_perfil,
+      // lojst_telefone:           createLojistaDto.lojst_telefone,
+      // lojst_email:              createLojistaDto.lojst_email,
+      // lojst_cep:                createLojistaDto.lojst_cep,
+      // lojst_endereco:           createLojistaDto.lojst_endereco,
+      // lojst_status:             createLojistaDto.lojst_status,
+      // lojst_loguin:             createLojistaDto.lojst_loguin,
+      // lojst_senha_hash:         createLojistaDto.lojst_senha_hash,
+      // lojst_token_inspiracao:   createLojistaDto.lojst_token_inspiracao,
+      // lojst_token_recuperacao:  createLojistaDto.lojst_token_recuperacao,
+      // cid_id:                   createLojistaDto.cid_id,
+      // bai_id:                   createLojistaDto.bai_id,
+      // est_id:                   createLojistaDto.est_id,
+      // lojst_loja_parceira:      createLojistaDto.lojst_loja_parceira
+      ...createLojistaDto
     } 
-    return this.prismaService.lojista.create({data});
+
+    await this.prismaService.lojista.create({data});
+
+    return {
+      message: "Usuário cadastrado com sucesso",
+      statusCode: 200 
+    }
   }
 
   findAll() {
@@ -36,7 +55,16 @@ export class LojistasService {
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} lojista`;
+    const data = this.prismaService.lojista.findUnique({
+      where: {
+        lojst_id: id
+      }
+    })
+
+    return {
+     data: data
+    }
+    
   }
 
   update(id: number, updateLojistaDto: UpdateLojistaDto) {
