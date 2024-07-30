7acb922d56be1e385e3495495c22d9418e377072
gabrielBartolace <gabrielll.freitas@hotmail.com>
2024-07-30 08:13:26 -0400

    validators lojista

--- src/lojistas/dto/create-lojista.dto.ts
+++ src/lojistas/dto/create-lojista.dto.ts
@@ -1,57 +1,72 @@
-import { IsNotEmpty, IsOptional } from "class-validator";
+import { IsDate, IsDateString, IsEmail, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
 import { LOJST_STATUS } from "../enums/lojst-status.enum";
-import { Exclude } from 'class-transformer';
+import { Exclude, Transform } from 'class-transformer';
 
 export class CreateLojistaDto {
-
+    @IsString()
     @IsNotEmpty()
     lojst_nome: string;
 
+    @IsString()
     @IsNotEmpty()
     lojst_cpf: string;
 
+    @IsString()
     @IsOptional()
     lojst_img_perfil?: string;
 
+    @IsString()
     @IsNotEmpty()
     lojst_telefone: string;
 
+    @IsEmail()
     @IsNotEmpty()
     lojst_email: string;
 
     @IsOptional()
     lojst_data_cadastro: Date;
 
+    @IsString()
     @IsOptional()
     lojst_cep?: string;
 
+    @IsString()
     @IsOptional()
     lojst_endereco?: string;
-
+    
+    @IsString()
     @IsOptional()
     lojst_status: LOJST_STATUS;
 
+    @IsString()
     @IsNotEmpty()
     lojst_loguin: string;
 
+    @IsString()
     @IsNotEmpty()
     lojst_senha_hash: string;
 
+    @IsString()
     @IsOptional()
     lojst_token_inspiracao?: string;
 
+    @IsString()
     @IsOptional()
     lojst_token_recuperacao?: string;
 
+    @IsNumber()
     @IsNotEmpty()
     cid_id: number;
 
+    @IsNumber()
     @IsNotEmpty()
     bai_id: number;
-    
+
+    @IsNumber()
     @IsNotEmpty()
     est_id: number;
 
+    @IsString()
     @IsOptional()
     lojst_loja_parceira?: string;
 }
